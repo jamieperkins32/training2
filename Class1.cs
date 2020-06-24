76e0e3353eb126ed11384908a3550ca3c601bd51
jamieperkins32 <67394113+jamieperkins32@users.noreply.github.com>
2020-06-24 22:26:19 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+
+
+namespace SeleniumTest
+
+    {
+    public class FirstTestCase
+    {
+        [Test]
+
+        public static void Main()
+        {
+
+            // Create a new instance of the Firefox driver
+            IWebDriver driver = new FirefoxDriver();
+
+            //Launch the Online Store Website
+            driver.Navigate().GoToUrl("http://www.google.com");
+
+            //Wait for 5 Sec
+            System.Threading.Thread.Sleep(5000);
+
+            // Close the driver
+            driver.Quit();
+        }
+    }
+
+}
+
+
+
